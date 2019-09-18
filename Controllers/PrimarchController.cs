cb1717e7f53ba3da9b69712b0f869c016ba220dd
TannorWallace <tannor.wallace@gmail.com>
2019-09-18 13:01:00 -0600

    I dont know 500 internal server error

--- Controllers/PrimarchController.cs
+++ Controllers/PrimarchController.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AstartesCodexProject.Models;
 using AstartesCodexProject.Data;
+using System.Collections.Generic;
+using AstartesCodexProject.Models;
 
 namespace AstartesCodexProject.Controllers
 {
