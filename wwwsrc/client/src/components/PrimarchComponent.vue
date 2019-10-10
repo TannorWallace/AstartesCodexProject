abee76cbadb4f792a14d62ba9ae6bc71d8ea6ee6
TannorWallace <tannor.wallace@gmail.com>
2019-10-09 19:08:35 -0600

    well I gotta study a bit on front end

---
+++ wwwsrc/client/src/components/PrimarchComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid">
+    <h2>The Primarchs</h2>
+    <div class="row Primarchs" v-for="Primarch in Primarch">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{Primarch.name}}</h5>
+          <p class="card-text">
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'PrimarchComponent',
+    data() {
+      return {}
+    },
+    // add a mounted for the store here?
+    computed: {},
+    methods: {},
+    components: {}
+  }
+</script>
+
+
+<style scoped>
+
+</style>
--- wwwsrc/client/src/router.js
+++ wwwsrc/client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// @ts-ignore
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -17,7 +18,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function () { 
+      component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "about" */ './views/About.vue')
       }
     }
